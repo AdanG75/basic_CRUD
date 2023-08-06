7b2d11b0a951f983155a4ae452deeb0119559fff
Adan Galicia <ansg75@gmail.com>
2023-08-05 18:17:55 -0600

    Add principal menu

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print(f'Client {client_name} has alredy been registered')
 
 
 def list_clients():
@@ -16,7 +20,24 @@ def _add_comma():
     clients += ','
 
 
+def print_welcome():
+    print('Welcome to Platzi Ventas')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
 if __name__ == '__main__':
-    list_clients()
-    create_client('david')
+    print_welcome()
+    command = input("Choice a option:\n-> ")
+
+    if command == 'C':
+        client_name = input("Give me your name, please:\n-> ")
+        create_client(client_name)
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
+
     list_clients()
