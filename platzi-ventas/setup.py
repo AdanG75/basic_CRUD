9aa2b6d0a3623905a62e232397a5c9dd87734e90
Adan Galicia <ansg75@gmail.com>
2023-08-14 14:51:28 -0600

    Begin platzi ventas

---
+++ platzi-ventas/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pv',
+    version='0.1.0',
+    py_modules=['pv'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+    [console_scripts]
+    pv=pv:cli
+    ''',
+)
