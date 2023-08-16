40ddc2a2f5d1393f84abee9d305a876a053675ad
Adan Galicia <ansg75@gmail.com>
2023-08-15 20:20:53 -0600

    Add create and update movement in pv

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -1,6 +1,10 @@
 import click
 
 
+from clients.services import ClientService
+from clients.models import Client as ClientModel
+
+
 @click.group()
 def clients():
     """Manages the clients lifecycle"""
@@ -8,17 +12,60 @@ def clients():
 
 
 @clients.command()
+@click.option(
+    '-n', 
+    '--name', 
+    type=str, 
+    prompt=True, 
+    help='The client name'
+    )
+@click.option(
+    '-c', 
+    '--company', 
+    type=str, 
+    prompt=True, 
+    help='The client company'
+    )
+@click.option(
+    '-e', 
+    '--email', 
+    type=str, 
+    prompt=True, 
+    help='The client email'
+    )
+@click.option(
+    '-p', 
+    '--position', 
+    type=str, 
+    prompt=True, 
+    help='The client position'
+    )
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Creates a new Client"""
-    pass
+    client = ClientModel(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_service.create_client(client)
 
 
 @clients.command()
 @click.pass_context
 def list(ctx):
     """List all clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    click.echo('  ID  |  NAME  |  COMPANY  |  EMAIL  |  POSITION')
+    click.echo('*' * 80)
+
+    for client in client_service.list_clients():
+        click.echo('{} | {} | {} | {} | {}'.format(
+            client['uid'],
+            client['name'],
+            client['company'],
+            client['email'],
+            client['position']
+        ))
 
 
 @clients.command()
